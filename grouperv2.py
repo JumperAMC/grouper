d754dedfc32f8bb6c8e1a77fda9d3ac9280f18ea
JumperAMC <58426915+JumperAMC@users.noreply.github.com>
2023-06-21 15:41:33 +0100

    new way of saving using streamlit.py

--- grouperv2.py
+++ grouperv2.py
@@ -3,6 +3,38 @@ import pandas as pd
 import streamlit as st
 import io
 
+buffer = io.BytesIO()
+
+
+@st.cache
+def convert_to_csv(df3):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv(index=False).encode('utf-8')
+
+csv = convert_to_csv(df3)
+
+# display the dataframe on streamlit app
+st.write(df3)
+
+# download button 1 to download dataframe as csv
+download1 = st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='large_df.csv',
+    mime='text/csv'
+)
+
+# download button 2 to download dataframe as xlsx
+with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+    # Write each dataframe to a different worksheet.
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+    download2 = st.download_button(
+        label="Download data as Excel",
+        data=buffer,
+        file_name='large_df.xlsx',
+        mime='application/vnd.ms-excel'
+    )
 
 def main():
     st.title("Student Grouping App")
