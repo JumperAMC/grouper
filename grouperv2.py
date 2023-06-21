95e053ef86dc6a3901626ad3e658667215876d20
JumperAMC <58426915+JumperAMC@users.noreply.github.com>
2023-06-21 15:26:12 +0100

    trying sd.write to output on screen grouperv2.py

--- grouperv2.py
+++ grouperv2.py
@@ -96,7 +96,7 @@ def main():
                             file_name=file_name,
                             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                         )
-                    print(df3)
+                    st.write(df3)
                 else:
                     st.warning("Please enter a file name.")
         
